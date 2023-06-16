8b6699107b5608d93c4b31118cfa9d13b1579fdf
BriannaFaith <faithbrianna01@gmail.com>
2023-06-16 11:02:31 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
-
 /**
- * main - Prints the numbers from 00 to 99, numbers separated by
- *        a comma followed by a space, in ascending order.
- *
- * Return: Always 0.
- */
+*main - Prints all combinations of two two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 0; digit1 <= 98; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
 		{
+			putchar((digit1 / 10) + '0');
 			putchar((digit1 % 10) + '0');
+			putchar(32);
+			putchar((digit2 / 10) + '0');
 			putchar((digit2 % 10) + '0');
 
-			if (digit1 == 9 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+			if (digit1 / 10 != 9 || digit1 % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 	}
-
-	putchar('\n');
-
+	putchar(10);
 	return (0);
 }
